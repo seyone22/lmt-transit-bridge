1720c87e7027ea8f5f8eb48471620142d64364ff
Seyone Gunasingham <s.g.seyone@proton.me>
2026-08-17 10:03:00 +0530

    perf: enable global HTTP/HTTPS TCP keep-alive socket reuse for Axios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import http from 'http';
+import https from 'https';
+import axios from 'axios';
+
+axios.defaults.httpAgent = new http.Agent({ keepAlive: true, maxSockets: 50 });
+axios.defaults.httpsAgent = new https.Agent({ keepAlive: true, maxSockets: 50 });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
